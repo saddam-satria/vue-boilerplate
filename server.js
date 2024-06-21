92f0a774f91abc8b3385aa6dfa7f9d28035083d1
saddam-satria <ardhisaddam@gmail.com>
2024-06-21 19:11:04 +0700

    fix: server

--- server.js
+++ server.js
@@ -7,30 +7,31 @@ const PORT = 3300
 Bun.serve({
     port: PORT,
     fetch(req) {
-
-
         const isBuildExist = existsSync(join("build"))
 
         if (!isBuildExist) {
             return new Response("failed to serve vue app")
 
         }
-
-        const mimetypes = [".json", ".png", ".jpg", ".png", ".ico", ".svg", ".pdf", ".txt", ".jpeg", ".JPG", ".JPEG", ".PNG", ".SVG"]
-
         const pathname = new URL(req.url).pathname
-        if (mimetypes.includes(extname(pathname))) {
-            const file = Bun.file(`./public${pathname}`)
-            return new Response(file)
+
+
+        const rootFile = Bun.file(join(__dirname, "build", "index.html"))
+        if (pathname === "/") {
+            return new Response(rootFile)
         }
 
-        if (pathname.includes("public")) {
-            const file = Bun.file(`./build${pathname}`)
-            return new Response(file)
+        const filePath = join(__dirname, "build", pathname)
+
+        if (!existsSync(filePath)) {
+            if (!extname(filePath)) {
+                return new Response(rootFile)
+            }
+            return new Response("not found")
         }
 
 
-        const file = Bun.file(join(__dirname, "build", "index.html"))
+        const file = Bun.file(filePath)
         return new Response(file)
     }
 })
