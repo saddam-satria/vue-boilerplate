b649857ba2a81ed55cf384d34f0ac679d407b94a
saddam-satria <ardhisaddam@gmail.com>
2024-06-21 23:26:53 +0700

    feat: env variabel vite

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { API_URL } from '@/utils/constant';
+</script>
 
 <template>
   <span class="text-red-300">about {{ $route.params.id }}</span>
+  <span class="text-red-300">about {{ API_URL }}</span>
 </template>
